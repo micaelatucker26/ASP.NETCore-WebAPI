1cf2380d8e97e0e0551e53b19d085d3f66dee4d9
Micaela Tucker <TUCKERMD@ETSU.EDU>
2022-04-09 19:26:07 -0400

    Clean up the UnitOfWork class to not use a logger.

--- MarketAPI/Classes/UnitOfWork.cs
+++ MarketAPI/Classes/UnitOfWork.cs
@@ -7,13 +7,13 @@ namespace MarketAPI.Classes
     public class UnitOfWork : IUnitOfWork
     {
         private readonly DataAccess _dataAccess;
-        private readonly ILogger _logger;
+        //private readonly ILogger _logger;
 
         public IItemRepository<Item> Item { get; private set; }
-        public UnitOfWork(DataAccess dataAccess, ILoggerFactory loggerFactory)
+        public UnitOfWork(DataAccess dataAccess) // ILoggerFactory loggerFactory
         {
             _dataAccess = dataAccess;
-            _logger = loggerFactory.CreateLogger("logs");
+            //_logger = loggerFactory.CreateLogger("logs");
             Item = new ItemRepository(_dataAccess); //(_dataAccess, _logger)
         }
     }
